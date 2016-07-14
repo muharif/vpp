1a366384172c4da4058a8445e0299f5fcbb490e7
muharif <muharif132@gmail.com>
2016-07-14 17:00:44 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -1995,9 +1995,9 @@ int class_add_del_class (class_main_t * cm,
 			  }
 		  }*/
 
-		  //if (add==0) {
-			//  for (j=1;j<=257;j++) {
-					  e->key[0][3]=e->key[0][3]+25;
+		  if (add==0) {
+			  for (j=1;j<=255;j++) {
+					  e->key[0][3]=e->key[0][3]+j;
 					  for (i = 0; i < t->match_n_vectors; i++) {
 						e->key[i] &= t->mask[i];
 					  };
@@ -2005,8 +2005,8 @@ int class_add_del_class (class_main_t * cm,
 					  if (rv)
 						return VNET_API_ERROR_NO_SUCH_ENTRY;
 
-			  //}
-		  //}
+			  }
+		  }
 
 	}
 	  return 0;

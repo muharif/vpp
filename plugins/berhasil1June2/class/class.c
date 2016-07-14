b88c75c832ee94dfb8f6830fd5e62154cf9e2d04
muharif <muharif132@gmail.com>
2016-07-14 15:25:10 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -1961,6 +1961,7 @@ for (add=0;add<=(field-1);add=add+1){
 
 	  for (i = 0; i < t->match_n_vectors; i++) {
 	    e->key[i] &= t->mask[i];
+	    e->key[i]++;
 	  };
 
       e->next_index = hit_next_index;

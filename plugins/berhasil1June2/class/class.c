08e6af35001e50223d25b10e4601179474ad5693
muharif <muharif132@gmail.com>
2016-07-15 11:54:25 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -1978,6 +1978,7 @@ int class_add_del_class (class_main_t * cm,
 		  //The conditions to expand if netmask is not 32,24 or 8 based on user input
 
 		  u8 test=0;
+		  u32 zero[8]={0};
 
 		   if (add==0) {
 			  if (add2==0) {
@@ -2038,7 +2039,7 @@ int class_add_del_class (class_main_t * cm,
 					  e->key[1][0] =temp+(256*j);
 					  for (i = 0; i < t->match_n_vectors; i++) {
 						e->key[i] &= t->mask[i];
-						if (e->key[i]=={0,0,0,0,0,0,0,0})
+						if (e->key[i]==zero)
 							test=test+1;
 					  };
 					  e->test=test;

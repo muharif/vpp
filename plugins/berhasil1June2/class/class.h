a7cf8aec5e0e4adda95c98b12b2818dcb6738a67
muharif <muharif132@gmail.com>
2016-07-15 17:20:14 +0200

    test

--- plugins/berhasil1June2/class/class.h
+++ plugins/berhasil1June2/class/class.h
@@ -72,8 +72,8 @@ typedef CLIB_PACKED(struct _class_entry {
     };
     u64 opaque_count;
     u32 next;
-    u32 id;
     struct {
+    	u32 id;
         u8 src;
         u8 dst;
         u8 proto;

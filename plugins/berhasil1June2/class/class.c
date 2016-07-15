ccdad2760fede5274456727abea9d5eede864709
muharif <muharif132@gmail.com>
2016-07-15 12:27:20 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -1978,7 +1978,7 @@ int class_add_del_class (class_main_t * cm,
 						}
 					  };
 					  if (input_check!=0)
-						  src=1;
+						  e->src=1;
 					  rv = class_add_del (t, e, is_add,table_index);
 					  if (rv)
 						return VNET_API_ERROR_NO_SUCH_ENTRY;

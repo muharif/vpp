27f906ee5b4bc43e56bf23a092f956928085a04e
muharif <muharif132@gmail.com>
2016-07-16 16:17:51 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -2114,15 +2114,18 @@ int class_add_del_class (class_main_t * cm,
 		  } else
 			  goto end_loop;
 
-		   h0 = (u8 *) e->key;
-		   h0 -= t->skip_n_vectors * sizeof (u32x4);
-		   hash0 = class_hash_packet (t, h0);
+		   if (is_add == 1) {
+			   h0 = (u8 *) e->key;
+			   h0 -= t->skip_n_vectors * sizeof (u32x4);
+			   hash0 = class_hash_packet (t, h0);
 
-		   e2 = class_find_entry (t, (u8 *) h0, hash0,
-		                                  now);
-		   if (e2->id < e->id && e2->next_index == e->next_index && e2->src == e->src
-				   && e2->dst == e->dst && e2->proto == e->proto && is_add==1)
-			   duplicate++;
+			   e2 = class_find_entry (t, (u8 *) h0, hash0,
+			                                  now);
+			   if (e2->id < e->id && e2->next_index == e->next_index && e2->src == e->src
+					   && e2->dst == e->dst && e2->proto == e->proto)
+				   duplicate++;
+		   }
+
 
 		   end_loop:
 

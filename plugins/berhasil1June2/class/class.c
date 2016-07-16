ffe020af85863a7f37bbcf6e5dd34d1faf3ad433
muharif <muharif132@gmail.com>
2016-07-16 21:10:18 +0200

    default

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -407,16 +407,16 @@ int class_add_del (class_table_t * t,
 
           if (!memcmp (v->key, add_v->key, t->match_n_vectors * sizeof (u32x4)))
             {
-              /*clib_memcpy (v, add_v, sizeof (class_entry_t) +
+              clib_memcpy (v, add_v, sizeof (class_entry_t) +
                       t->match_n_vectors * sizeof(u32x4));
               v->flags &= ~(CLASS_ENTRY_FREE);
 
               CLIB_MEMORY_BARRIER();
-              b->as_u64 = t->saved_bucket.as_u64;*/
+              b->as_u64 = t->saved_bucket.as_u64;
               goto unlock;
             }
         }
-      /*for (i = 0; i < t->entries_per_page; i++)
+      for (i = 0; i < t->entries_per_page; i++)
         {
           v = class_entry_at_index (t, save_v, value_index + i);
 
@@ -430,7 +430,7 @@ int class_add_del (class_table_t * t,
               t->active_elements ++;
               goto unlock;
             }
-        }*/
+        }
       /* no room at the inn... split case... */
     }
   else

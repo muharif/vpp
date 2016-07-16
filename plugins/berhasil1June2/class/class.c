4af92682a5e4566b3455d0e4057bf15a77733b20
muharif <muharif132@gmail.com>
2016-07-16 20:27:57 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -2165,7 +2165,7 @@ int class_add_del_class (class_main_t * cm,
 	}
 	u32 in;
 	t = pool_elt_at_index (cm->tables, in);
-	memcpy(&t->next, &n, sizeof(n));
+	memcpy( &n, &t->next, sizeof(n));
 	reset_next(n);
 
 	  return 0;

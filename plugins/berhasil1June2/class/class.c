8d0756f487c519149bbc3965103bd44b2127bfa1
muharif <muharif132@gmail.com>
2016-07-16 21:04:07 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -407,12 +407,12 @@ int class_add_del (class_table_t * t,
 
           if (!memcmp (v->key, add_v->key, t->match_n_vectors * sizeof (u32x4)))
             {
-              clib_memcpy (v, add_v, sizeof (class_entry_t) +
+              /*clib_memcpy (v, add_v, sizeof (class_entry_t) +
                       t->match_n_vectors * sizeof(u32x4));
               v->flags &= ~(CLASS_ENTRY_FREE);
 
               CLIB_MEMORY_BARRIER();
-              b->as_u64 = t->saved_bucket.as_u64;
+              b->as_u64 = t->saved_bucket.as_u64;*/
               goto unlock;
             }
         }

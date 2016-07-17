f80fcb5d8f52cef882c9fdf3e151486f8c360fdd
muharif <muharif132@gmail.com>
2016-07-17 15:24:58 +0200

    ebst

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -207,14 +207,13 @@ class_node_fn (vlib_main_t * vm,
 	              if (!e0) {
 	            	  checkempty:
 	            	  table_index0++;
-		              t0 = pool_elt_at_index (vcm->tables, table_index0);
 
 
-	            	  if (((t0->table_index) - x) > 8)
+	            	  if ((table_index0 - x) == 8)
 	            		  goto process;
 
-
-	            	  if (t0->active_elements==0){
+		              t0 = pool_elt_at_index (vcm->tables, table_index0);
+		              if (t0->active_elements==0){
 	            		  goto checkempty;
 	            	  } else if (t0->active_elements>0) {
 	            			  goto loop;

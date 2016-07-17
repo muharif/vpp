e4fba7f5d4c81ea519df387e656f7ecbefbbfd95
muharif <muharif132@gmail.com>
2016-07-17 16:17:39 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -307,14 +307,13 @@ class_node_fn (vlib_main_t * vm,
 					  if ((n->src == temp->srcid) && (n->dst == temp->dstid) && (n->proto == temp->proto)) {
 						  next0 = n->action;
 						  clear_temp (temp);
-						  n=0;
 						  goto end;
 					  } else {
 						  next0 = 0;
-						  n=0;
-						  clear_temp (temp);
 					  }
 				  }
+				  clear_temp (temp);
+
 
 			  } else {
 				  next0 = 11;

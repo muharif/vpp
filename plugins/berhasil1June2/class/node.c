1cf3f8eeb4bf010ec70cfc0613ac6fb8b80d936f
muharif <muharif132@gmail.com>
2016-07-17 16:13:13 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -303,9 +303,9 @@ class_node_fn (vlib_main_t * vm,
 				  i=0;
 				  for (i=0;i<=100;i++) {
 					  n = pool_elt_at_index (vcm->next, i);
-					  test = n->action;
+					  test = n->src;
 					  if ((n->src == temp->srcid) && (n->dst == temp->dstid) && (n->proto == temp->proto)) {
-						  next0 = test;
+						  next0 = n->action;
 						  clear_temp (temp);
 						  n=0;
 						  goto end;

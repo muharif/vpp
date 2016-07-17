e67a98e1536bbbdb5d708ebb8e0e891a3ec95e23
muharif <muharif132@gmail.com>
2016-07-17 12:37:17 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -252,7 +252,9 @@ class_node_fn (vlib_main_t * vm,
 
 			  // check identifier
 			  if (e0) {
-	        	  if ((table_index0-x)<=4 && (table_index0-x)>0) {
+				  if (table_index == 0)
+					  next_table == x+1;
+				  else if ((table_index0-x)<=4 && (table_index0-x)>0) {
 	        		  temp->srcid = e0->id;
 	        		  next_table = x+5;
 	        	  }
@@ -263,8 +265,6 @@ class_node_fn (vlib_main_t * vm,
 	        	  else if ((table_index0-x) == x+field) {
 	        		  temp->protoid = e0->id;
 	        		  next_table = 0;
-	        	  } else {
-	        		  next_table = x+1;
 	        	  }
 			  } else {
 				  temp->protoid = 0;

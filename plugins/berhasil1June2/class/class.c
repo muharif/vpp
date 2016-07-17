f79eeed60da30a7a0f860d44dd015de7adddf366
muharif <muharif132@gmail.com>
2016-07-17 13:32:52 +0200

    test

--- plugins/berhasil1June2/class/class.c
+++ plugins/berhasil1June2/class/class.c
@@ -1894,7 +1894,7 @@ class_check_input_t * class_check (class_main_t * cm, class_entry_t * e, u8 * ma
 void clear_temp (class_temp_t * temp) {
 	temp->srcid = 0;
 	temp->dstid = 0;
-	temp->protoid = 0;
+	temp->proto = 0;
 }
 
 class_next_t *

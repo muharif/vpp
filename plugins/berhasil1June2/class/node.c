f44365bf3af41b54fb627af9ae97eeb478be95bd
muharif <muharif132@gmail.com>
2016-07-17 14:27:02 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -206,7 +206,7 @@ class_node_fn (vlib_main_t * vm,
 	            	  checkempty:
 	            	  table_index0++;
 
-	            	  if ((table_index0 - x) == field)
+	            	  if ((table_index0 - x) == 9)
 	            		  goto process;
 
 		              t0 = pool_elt_at_index (vcm->tables, table_index0);

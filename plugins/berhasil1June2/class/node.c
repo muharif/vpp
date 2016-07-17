1b4942f91eb609d466cf32c5dd72c3b63fa55cab
muharif <muharif132@gmail.com>
2016-07-17 13:52:32 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -277,8 +277,8 @@ class_node_fn (vlib_main_t * vm,
 			  vnet_buffer(b0)->l2_classify.table_index=next_table;
 
 			  if (next_table == 0) {
-				  if (!e0)
-					  return 0;
+				  //if (!e0)
+				//	  return 0;
 
 				  for (i=0;i<=100;i++) {
 					  n = pool_elt_at_index (vcm->next, i);

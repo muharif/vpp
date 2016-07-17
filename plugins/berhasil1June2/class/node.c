fa53d6137862be75e5b9ee5a345b16b15ba0547c
muharif <muharif132@gmail.com>
2016-07-17 15:23:12 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -210,7 +210,7 @@ class_node_fn (vlib_main_t * vm,
 		              t0 = pool_elt_at_index (vcm->tables, table_index0);
 
 
-	            	  if (((t0->table_index) - x) > 9)
+	            	  if (((t0->table_index) - x) > 8)
 	            		  return 0;
 
 

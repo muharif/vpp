99b55144330d4dc40b92548fd0f1ce3f80f82594
muharif <muharif132@gmail.com>
2016-07-18 12:19:51 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -207,7 +207,7 @@ class_node_fn (vlib_main_t * vm,
 
 	              if (!e0) {
 	            	  checkempty:
-	            	  if ((table_index0 - x) > field)
+	            	  if ((table_index0 - x) == field)
 	            		  goto process;
 
 	            	  table_index0++;

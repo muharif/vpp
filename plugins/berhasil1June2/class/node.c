860b59cf9fa4baca7401ea8ab6160f7a63485998
muharif <muharif132@gmail.com>
2016-07-18 11:37:25 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -276,11 +276,11 @@ class_node_fn (vlib_main_t * vm,
 	        		  temp->srcid = 0;
 	        		  temp->dstid = 0;
 	        		  temp->proto = 0;
-	        	  }/* else {
+	        	  } else {
 	        		  temp->srcid = 0;
 	        		  temp->dstid = 0;
 	        		  temp->proto = 0;
-	        	  }*/
+	        	  }
 
 				  next_table = 0;
 			  } else {
@@ -306,6 +306,9 @@ class_node_fn (vlib_main_t * vm,
 				  i=0;
 				  for (i=0;i<=100;i++) {
 					  n = pool_elt_at_index (vcm->next, i);
+					  if (temp->srcid == 0 && temp->dstid == 0 && temp->proto == 0)
+						  return 0;
+
 					  if ((n->src == temp->srcid) && (n->dst == temp->dstid) && (n->proto == temp->proto)) {
 						  next0 = n->action;
 						  goto end;

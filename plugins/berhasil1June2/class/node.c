1510be0692ef300596a3941d06f0745bfaa24186
muharif <muharif132@gmail.com>
2016-07-18 12:58:17 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -286,7 +286,7 @@ class_node_fn (vlib_main_t * vm,
 				  } else {
 					  if ((table_index0-x) <=4 && (table_index0-x)>0) {
 		        		  temp->srcid = e0->id;
-		        		  next_table = 5;
+		        		  next_table = x+5;
 		        	  } else if ((table_index0-x) <= 8 && (table_index0-x) > 4) {
 		        		  temp->dstid = e0->id;
 		        		  next_table = x+field;

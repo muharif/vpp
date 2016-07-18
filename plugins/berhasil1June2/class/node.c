408ebdf273b2c7f8fb4a5d27c0ca2d2fada15197
muharif <muharif132@gmail.com>
2016-07-18 21:22:02 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -339,7 +339,7 @@ class_node_fn (vlib_main_t * vm,
 								   to_next, n_left_to_next,
 								   bi0, next0);
 		}
-	    	  //vlib_put_next_frame (vm, node, next_index, n_left_to_next);
+	    	  vlib_put_next_frame (vm, node, next_index, n_left_to_next);
 	    }
 
 	  vlib_node_increment_counter (vm, node->node_index,

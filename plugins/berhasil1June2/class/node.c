af47f1aee5c808b5df048f315b83d57718e974b8
muharif <muharif132@gmail.com>
2016-07-18 20:11:21 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -265,7 +265,7 @@ class_node_fn (vlib_main_t * vm,
 			  next_table = 0;
 
 			  if (!e0) {
-				  id=0;
+				  id=table_index0-x;
             	  //if ((table_index0 - x) == field) {
     				  if (!(temp->srcid))
     					  temp->srcid = 0;
@@ -291,7 +291,8 @@ class_node_fn (vlib_main_t * vm,
 		        	  }
 				  }
 				  vnet_buffer(b0)->l2_classify.table_index=next_table;
-				  id=e0->id;
+				  //id=e0->id;
+				  id=table_index0-x;
 			  }
 
 			  if (next_table == 0) {

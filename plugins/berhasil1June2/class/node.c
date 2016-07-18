685a2749de78b11bf4c635d4512375e2c6fbfd14
muharif <muharif132@gmail.com>
2016-07-18 17:04:08 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -276,6 +276,7 @@ class_node_fn (vlib_main_t * vm,
 				  id = 0;
 				  test = 1;
 	        	  next_table = 0;
+	        	  next0 = 0;
 	        	  goto end;
 			  } else {
 				  if (table_index0 == 0) {

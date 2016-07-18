ccd66a34949bbd1922fae7da1f85bb5355823875
muharif <muharif132@gmail.com>
2016-07-18 19:02:57 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -272,7 +272,7 @@ class_node_fn (vlib_main_t * vm,
 			  if (!e0) {
 				  id=0;
 
-				  if ((table_index0-x) == 0) {
+				  if ((table_index0-x) <= 0) {
 					  temp->srcid = 0;
 					  temp->dstid = 0;
 	        		  temp->proto = 0;

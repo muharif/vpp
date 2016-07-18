90fddea66d95af40318e501bb359717bbe5ea687
muharif <muharif132@gmail.com>
2016-07-18 21:16:07 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -207,7 +207,6 @@ class_node_fn (vlib_main_t * vm,
 	            	  checkempty:
 
 	            	  if ((table_index0 - x) == 0) {
-	            		  return 0;
 	            		  goto process;
 	            	  }
 
@@ -320,62 +319,6 @@ class_node_fn (vlib_main_t * vm,
 			  }
 
 			  end:
-
-              //Check only the field that want to be checked
-
-	          /*if (table_index0==0) {
-	        	  if (e0->src==0) {
-	        		  if (e0->dst==0){
-						  if (e0->proto==0) {
-							  next_table=0;
-						  } else
-							  next_table=x+field;
-					  } else
-						  next_table=x+5;
-	        	  } else
-	        		  next_table = x+1;
-	          } else if ((table_index0-x)<=4 && (table_index0-x)>0) {
-	        	  if (e0->dst==0){
-	        		  if (e0->proto==0) {
-	        			  next_table=0;
-	        		  } else
-	        			  next_table=x+field;
-	        	  } else
-	        		  next_table=x+5;
-	          } else if ((table_index0-x)<=8 && (table_index0-x)>4) {
-	        	  if (e0->proto==0)
-	        		  next_table=0;
-	        	  else
-	        		  next_table=x+field;
-	          }*/
-
-	          //Deciding next step
-
-	          /*if (next_table !=0) {
-
-	        	  vnet_buffer(b0)->l2_classify.table_index=next_table;
-	        	  if (table_index0 !=0){
-					  if (temp->prev==0) {
-						  next0=11;
-						  temp->prev=e0->id;
-					  }
-					  else if (temp->prev!=e0->id) {
-						  next0=0;
-						  temp->prev=0;
-
-					  }
-					  else
-						  next0=11;
-	        	  }
-
-	          } else {
-	        	  if (((e0->src)+(e0->dst)+(e0->proto)) != 1){
-	        		  if (temp->prev != e0->id)
-	        			  next0=0;
-	        		  	  temp->prev=0;
-	        	  }
-	          }*/
-
 			  id = not_found;
 
 	          if (PREDICT_FALSE((node->flags & VLIB_NODE_FLAG_TRACE)
@@ -385,16 +328,14 @@ class_node_fn (vlib_main_t * vm,
 	                vlib_add_trace (vm, node, b0, sizeof (*t));
 	              t->id = id;
 	              t->next_index = next0;
-	              //t->table_index = t0 ? t0 - vcm->tables : ~0;
 	              t->table_index = table_index0;
 	            }
 
 	          /* verify speculative enqueue, maybe switch current next frame */
-	          if (not_found != 1) {
+	          if (id != 1)
 				  vlib_validate_buffer_enqueue_x1 (vm, node, next_index,
 								   to_next, n_left_to_next,
 								   bi0, next0);
-	          }
 		}
 	    	  vlib_put_next_frame (vm, node, next_index, n_left_to_next);
 	    }

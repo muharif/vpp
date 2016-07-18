38fb09c49ca6027a88d2c209402614289228d274
muharif <muharif132@gmail.com>
2016-07-18 13:52:13 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -304,7 +304,7 @@ class_node_fn (vlib_main_t * vm,
 		        	  }
 				  }
 				  vnet_buffer(b0)->l2_classify.table_index=next_table;
-				  id=e0->id;
+				  //id=e0->id;
 			  }
 
 			  if (next_table == 0) {

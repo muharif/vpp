1d4e2233ad1fe1c8cc68d545a4c7f4ba39cdebc0
muharif <muharif132@gmail.com>
2016-07-18 14:32:49 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -271,10 +271,12 @@ class_node_fn (vlib_main_t * vm,
 				  /*if ((table_index0-x) <= 8 && (table_index0-x) > 4) {
 	        		  temp->srcid = 0;
 	        	  } else */ if ((table_index0-x) == 0) {
-	        		  if (!(temp->srcid > 0))
-	        			  temp->srcid = 0;
-	        		  if (!(temp->dstid > 0))
-	        			  temp->dstid = 0;
+	        		  if (temp->srcid > 0)
+	        		  else
+	        			  temp->srcid =0;
+	        		  if (temp->dstid > 0)
+	        		  else
+	        			  temp->dstid =0;
 	        		  temp->proto = 0;
 	        	  }
 

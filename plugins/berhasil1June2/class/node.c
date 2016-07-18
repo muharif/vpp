c6f3ebbe440f0aedcc5924ef20a60e33ed9383c9
muharif <muharif132@gmail.com>
2016-07-18 21:18:55 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -336,7 +336,6 @@ class_node_fn (vlib_main_t * vm,
 								   to_next, n_left_to_next,
 								   bi0, next0);
 		}
-	      end2:
 	    	  //vlib_put_next_frame (vm, node, next_index, n_left_to_next);
 	    }
 

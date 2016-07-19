47a873cc769d097617972db388e5705368ac5ddc
muharif <muharif132@gmail.com>
2016-07-19 12:58:33 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -189,8 +189,7 @@ class_node_fn (vlib_main_t * vm,
 	          e0 = 0;
 	          t0 = 0;
 	          vnet_buffer(b0)->l2_classify.opaque_index = ~0;
-	          x0=table_index0/field;
-	          x=x0*field;
+
 
 
 
@@ -206,6 +205,9 @@ class_node_fn (vlib_main_t * vm,
 
 	              if (!e0) {
 	            	  checkempty:
+			          x0=table_index0/field;
+			          x=x0*field;
+
 		              test = (table_index0 - x);
 
 

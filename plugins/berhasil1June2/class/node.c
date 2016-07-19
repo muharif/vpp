21b6cca766eedec00f66ff78b039299c68ccf804
muharif <muharif132@gmail.com>
2016-07-19 11:34:10 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -274,7 +274,7 @@ class_node_fn (vlib_main_t * vm,
 			  if (!e0) {
 				  id=x;
 
-				  if ((table_index0-x) == 0) {
+				  if ((table_index0-x) == field) {
 					  //if (!(temp->srcid))
 						  temp->srcid = 0;
 					  //if (!(temp->dstid))

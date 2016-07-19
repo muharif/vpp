324ab21d019fd0175fceeb50f0861387b6f81080
muharif <muharif132@gmail.com>
2016-07-19 13:34:35 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -206,16 +206,16 @@ class_node_fn (vlib_main_t * vm,
 	              if (!e0) {
 	            	  checkempty:
 
-					  if ((table_index0 - x) <=8 && (table_index0 - x) > 0) {
-		            	  table_index0++;
-			              t0 = pool_elt_at_index (vcm->tables, table_index0);
-			              if (t0->active_elements == ~0 || t0->active_elements == 0){
-		            		  goto checkempty;
-		            	  } else if (t0->active_elements>0) {
-		            			  goto loop;
-		            	  }
-					  } else
-						  goto process;
+					  if ((table_index0 - x) == field)
+					  	  goto process;
+
+	            	  table_index0++;
+		              t0 = pool_elt_at_index (vcm->tables, table_index0);
+		              if (t0->active_elements == ~0 || t0->active_elements == 0){
+	            		  goto checkempty;
+	            	  } else if (t0->active_elements>0) {
+	            			  goto loop;
+	            	  }
 	              }
 
 	              if (e0)

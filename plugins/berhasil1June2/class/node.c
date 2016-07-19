021c8818acf71ae02188aed6e7581122857d97aa
muharif <muharif132@gmail.com>
2016-07-19 12:01:07 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -390,9 +390,11 @@ class_node_fn (vlib_main_t * vm,
 	            }
 
 	          /* verify speculative enqueue, maybe switch current next frame */
-		  /*vlib_validate_buffer_enqueue_x1 (vm, node, next_index,
+	         if (e0) {
+		  vlib_validate_buffer_enqueue_x1 (vm, node, next_index,
 						   to_next, n_left_to_next,
-						   bi0, next0);*/
+						   bi0, next0);
+	         }
 		}
 
 	      vlib_put_next_frame (vm, node, next_index, n_left_to_next);

56990e447eec2e016d0f482bd5989f7004e0aaa1
muharif <muharif132@gmail.com>
2016-07-19 13:10:22 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -216,7 +216,7 @@ class_node_fn (vlib_main_t * vm,
 
 	            	  table_index0++;
 		              t0 = pool_elt_at_index (vcm->tables, table_index0);
-		              if (t0->active_elements==0){
+		              if (t0->active_elements == ~0 || t0->active_elements == 0){
 	            		  goto checkempty;
 	            	  } else if (t0->active_elements>0) {
 	            			  goto loop;

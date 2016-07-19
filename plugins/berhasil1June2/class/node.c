ca330fe5e844bbf0aac22d2dbe7af23e3475773a
muharif <muharif132@gmail.com>
2016-07-19 11:40:59 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -90,7 +90,7 @@ class_node_fn (vlib_main_t * vm,
 	  u32 misses = 0;
 	  u32 chain_hits = 0;
 	  int field=9;
-	  u32 x0,x;
+	  int x0,x;
 	  int i;
 	  u32 next_table;
 	  class_temp_t * temp = &class_temp;
@@ -188,7 +188,7 @@ class_node_fn (vlib_main_t * vm,
 	          e0 = 0;
 	          t0 = 0;
 	          vnet_buffer(b0)->l2_classify.opaque_index = ~0;
-	          x0=floor(table_index0/field);
+	          x0=table_index0/field;
 	          x=x0*field;
 
 
@@ -205,7 +205,7 @@ class_node_fn (vlib_main_t * vm,
 	              if (!e0) {
 	            	  checkempty:
 
-	            	  if ((table_index0 - x) == 0)
+	            	  if ((table_index0 - x) == field)
 	            		  goto process;
 
 	            	  table_index0++;

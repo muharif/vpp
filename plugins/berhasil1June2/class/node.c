eaa8c932deb9457be62c9ca075e98e9b8790e4ae
muharif <muharif132@gmail.com>
2016-07-19 13:33:52 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -206,7 +206,7 @@ class_node_fn (vlib_main_t * vm,
 	              if (!e0) {
 	            	  checkempty:
 
-					  if ((table_index0 - x) <=8) {
+					  if ((table_index0 - x) <=8 && (table_index0 - x) > 0) {
 		            	  table_index0++;
 			              t0 = pool_elt_at_index (vcm->tables, table_index0);
 			              if (t0->active_elements == ~0 || t0->active_elements == 0){

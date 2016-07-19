6e0cd34792af9200b24a32e9f84013a0139d61d5
muharif <muharif132@gmail.com>
2016-07-19 12:56:54 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -201,12 +201,13 @@ class_node_fn (vlib_main_t * vm,
 	              t0 = pool_elt_at_index (vcm->tables, table_index0);
 	              e0 = class_find_entry (t0, (u8 *) h0, hash0,
 	                                             now);
-	              test = (table_index0 - x);
 
 	              //Check next table if entry can't be found
 
 	              if (!e0) {
 	            	  checkempty:
+		              test = (table_index0 - x);
+
 
 					  if ((table_index0 - x) == field)
 					  	            		  goto process;

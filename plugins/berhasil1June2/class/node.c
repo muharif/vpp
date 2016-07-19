529c2ef3b8e70757bad5258f2d9a03f81963d4c2
muharif <muharif132@gmail.com>
2016-07-19 12:35:25 +0200

    test

--- plugins/berhasil1June2/class/node.c
+++ plugins/berhasil1June2/class/node.c
@@ -295,7 +295,8 @@ class_node_fn (vlib_main_t * vm,
 		        	  }
 				  }
 				  vnet_buffer(b0)->l2_classify.table_index=next_table;
-				  id=e0->id;
+				  //id=e0->id;
+				  id = 100;
 			  }
 
 			  if (next_table == 0) {

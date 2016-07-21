4a4d5b7f5cb121e3b066c2451e0ed00d9767b489
muharif <muharif132@gmail.com>
2016-07-21 12:52:09 +0200

    test

--- plugins/berhasil19Juli/class/class.c
+++ plugins/berhasil19Juli/class/class.c
@@ -2231,7 +2231,7 @@ int class_add_del_class (class_main_t * cm,
 					return VNET_API_ERROR_NO_SUCH_ENTRY;
 		  } else if (add == 3) {
 			  u32 temp=e->key[1][1];
-			  //e->key[1][1] =0;
+			  e->key[1][1] =0;
 			  e->key[1][1] =temp+(16777216*srcport);
 			  for (i = 0; i < t->match_n_vectors; i++) {
 					e->key[i] &= t->mask[i];
@@ -2243,7 +2243,7 @@ int class_add_del_class (class_main_t * cm,
 					return VNET_API_ERROR_NO_SUCH_ENTRY;
 		  } else if (add == 4) {
 			  u32 temp=e->key[1][2];
-			  //e->key[1][2] =0;
+			  e->key[1][2] =0;
 			  e->key[1][2] =temp+(256*dstport);
 			  for (i = 0; i < t->match_n_vectors; i++) {
 					e->key[i] &= t->mask[i];

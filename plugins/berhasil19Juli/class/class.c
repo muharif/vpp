536afe1441eadc1118ed08940d0b0b73e360c5bf
muharif <muharif132@gmail.com>
2016-07-21 11:57:47 +0200

    test

--- plugins/berhasil19Juli/class/class.c
+++ plugins/berhasil19Juli/class/class.c
@@ -2161,8 +2161,7 @@ int class_add_del_class (class_main_t * cm,
 
 			  if (add2==4) {
 				  mult=32-dstmask;
-				  e->key[1][1] =e->key[1][1]+(16777216*dstport);
-				  e->key[1][1] =e->key[1][1]+(256*srcport);
+				  e->key[1][1] =e->key[1][1]+1;
 				  u32 temp=e->key[1][0];
 				  for (j=0;j<(pow(2,mult));j++) {
 					  e->key[1][0] = temp+(256*j);

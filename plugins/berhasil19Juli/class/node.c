6a04caea3e72a74ce532031baa50e5aeb54612ef
muharif <muharif132@gmail.com>
2016-07-22 10:47:28 +0200

    test

--- plugins/berhasil19Juli/class/node.c
+++ plugins/berhasil19Juli/class/node.c
@@ -399,8 +399,10 @@ class_node_fn (vlib_main_t * vm,
 	              t->time=time_spent;
 	            }
 
-	          if (next_table !=0)
+	          if (next_table !=0) {
+	        	  temp->num = 0;
 	        	  goto begin;
+	          }
 
 	          /* verify speculative enqueue, maybe switch current next frame */
 
